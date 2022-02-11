5148be3d65e7ed53943a861dd365d593f20de800
melinnediniz <melinnediniz@gmail.com>
2022-02-11 18:09:40 -0400

    [IMPROVEMENT] Added 'Start Page', 'Exit' options and changed background

--- pong.py
+++ pong.py
@@ -1,3 +1,4 @@
+from tkinter import mainloop
 import turtle
 import winsound
 import random
@@ -7,148 +8,233 @@ FONT = "Press Start 2P"
 UP_DOWN_SOUND = "assets/bounce.wav"
 SCORE_SOUND = "assets/258020__kodack__arcade-bleep-sound.wav"
 
-# draw window
-screen = turtle.Screen()
-screen.title("My Pong")
-screen.bgcolor("black")
-screen.setup(width=800, height=600)
-screen.tracer(0)
-
-# draw paddle 1
-paddle_1 = turtle.Turtle()
-paddle_1.speed(0)
-paddle_1.shape("square")
-paddle_1.color("white")
-paddle_1.shapesize(stretch_wid=5, stretch_len=1)
-paddle_1.penup()
-paddle_1.goto(-350, 0)
-
-# draw paddle 2
-paddle_2 = turtle.Turtle()
-paddle_2.speed(0)
-paddle_2.shape("square")
-paddle_2.color("white")
-paddle_2.shapesize(stretch_wid=5, stretch_len=1)
-paddle_2.penup()
-paddle_2.goto(350, 0)
-
-# draw ball
-ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("circle")
-ball.color("white")
-ball.penup()
-ball.goto(0, 0)
-ball.dx = 0.40
-ball.dy = 0.40
-
-# scores
-score_1 = 0
-score_2 = 0
-score_win = 10
-
-# score head-up display
-hud = turtle.Turtle()
-hud.speed(0)
-hud.shape("square")
-hud.color("white")
-hud.penup()
-hud.hideturtle()
-hud.goto(0, 250)
-hud.write("0 : 0", align="center", font=(FONT, 24, "normal"))
+start_pg = turtle.Screen()
+start_pg.bgcolor('black')
+start_pg.setup(width=800, height=600)
 
 
-# move paddle up
-def move_up(paddle):
-    y = paddle.ycor()
-    if y < 250:
-        y += 20
-    else:
-        y = 250
-    paddle.sety(y)
+# draw first window
+def start_page():
+    
+    # draw game name in the top
+    game_name = turtle.Turtle()
+    game_name.speed(0)
+    game_name.hideturtle()
+    game_name.penup()
+    game_name.color("white")
+    game_name.goto(0, 160)
+    game_name.write('PING PONG', align="center", font=(FONT, 50, 'normal'))
+
+    # draw start button
+    start_button = turtle.Turtle()
+    start_button.speed(0)
+    start_button.hideturtle()
+    start_button.pensize(4)
+    start_button.color('#BFDEB4')
+    start_button.penup()
+    start_button.goto(-112, -20)
+
+    # draw button borders
+    start_button.fillcolor('#0A1B08')
+    start_button.begin_fill()
+    for _ in range(2):
+        start_button.pendown()
+        start_button.forward(220)
+        start_button.left(90)
+        start_button.forward(70)
+        start_button.left(90)
+    start_button.end_fill()
+    
+    start_button.penup()
+    start_button.goto(0, 0)
+    start_button.write('Start Game', align='center', font=(FONT, 15, 'normal'))
+
+    exit_button = turtle.Turtle()
+    exit_button.speed(0)
+    exit_button.hideturtle()
+    exit_button.pensize(4)
+    exit_button.color('red')
+    exit_button.penup()
+    exit_button.goto(0, -100)
+    exit_button.write('Exit Game', align='center', font=(FONT, 15, 'normal'))
 
 
-# move paddle down
-def move_down(paddle):
-    y = paddle.ycor()
-    if y > -250:
-        y += -20
-    else:
-        y = -250
-    paddle.sety(y)
+# function to start or exit game
+def button_actions(x, y):
+    global start_pg
+    if -110 < x < 100 and 0 < y < 40:
+        print('Start Game')
+        # winsound.PlaySound('assets/wining.wav', winsound.SND_ASYNC)
+        start_pg.clear()
+        main()
+    elif -85 < x < 82 and -105 < y < -62:
+        print('exit game')
+        turtle.bye()
 
 
-# paddles movement
-def paddle_1_up():
-    move_up(paddle_1)
+# calling the function
+turtle.listen()
+turtle.onscreenclick(button_actions, 1)
 
 
-def paddle_1_down():
-    move_down(paddle_1)
+def main():
 
+    # draw window    
+    screen = turtle.Screen()
+    screen.title("My Pong")
+    screen.bgcolor("black")
+    screen.setup(width=800, height=600)
+    screen.tracer(0)
+    screen.bgpic('assets/game_background.png')
+    
+    # scores
+    score_1 = 0
+    score_2 = 0
+    score_win = 10
 
-def paddle_2_up():
-    move_up(paddle_2)
+    # draw paddle 1
+    paddle_1 = turtle.Turtle()
+    paddle_1.speed(0)
+    paddle_1.shape("square")
+    paddle_1.color("#CD7011")
+    paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+    paddle_1.penup()
+    paddle_1.goto(-350, 0)
 
+    # draw paddle 2
+    paddle_2 = turtle.Turtle()
+    paddle_2.speed(0)
+    paddle_2.shape("square")
+    paddle_2.color("#16899E")
+    paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+    paddle_2.penup()
+    paddle_2.goto(350, 0)
 
-def paddle_2_down():
-    move_down(paddle_2)
+    # draw ball
+    ball = turtle.Turtle()
+    ball.speed(0)
+    ball.shape("circle")
+    ball.color("#8BD580")
+    ball.penup()
+    ball.goto(0, 0)
+    ball.dx = 0.40
+    ball.dy = 0.40
 
+    # score head-up display
+    hud = turtle.Turtle()
+    hud.speed(0)
+    hud.shape("square")
+    hud.color("white")
+    hud.penup()
+    hud.hideturtle()
+    hud.goto(0, 250)
+    hud.write("0 : 0", align="center", font=(FONT, 24, "normal"))
 
-# mapping keys
-screen.listen()
-screen.onkeypress(paddle_1_up, "w")
-screen.onkeypress(paddle_1_down, "s")
-screen.onkeypress(paddle_2_up, "Up")
-screen.onkeypress(paddle_2_down, "Down")
+    # draw exit warning
+    exit_warning = turtle.Turtle()
+    exit_warning.speed(0)
+    exit_warning.hideturtle()
+    exit_warning.color("red")
+    exit_warning.penup()
+    exit_warning.goto(0, -295)
+    exit_warning.write("PRESS 'SPACE' TO EXIT", align="center", font=("Small Fonts", 12, "normal"))
 
-while True:
-    screen.update()
+    # move paddle up
+    def move_up(paddle):
+        y = paddle.ycor()
+        if y < 250:
+            y += 20
+        else:
+            y = 250
+        paddle.sety(y)
 
-    # ball move
-    ball.setx(ball.xcor() + ball.dx)
-    ball.sety(ball.ycor() + ball.dy)
+    # move paddle down
+    def move_down(paddle):
+        y = paddle.ycor()
+        if y > -250:
+            y += -20
+        else:
+            y = -250
+        paddle.sety(y)
 
-    # collision with top wall
-    if ball.ycor() > 285:
-        ball.sety(285)
-        ball.dy *= -1
-        winsound.PlaySound(UP_DOWN_SOUND,  winsound.SND_ASYNC)
+    # paddles movement
+    def paddle_1_up():
+        move_up(paddle_1)
 
-    # collision with down wall
-    if ball.ycor() < -280:
-        ball.sety(-280)
-        ball.dy *= -1
-        winsound.PlaySound(UP_DOWN_SOUND,  winsound.SND_ASYNC)
+    def paddle_1_down():
+        move_down(paddle_1)
 
-    # collision with left wall
-    if ball.xcor() < -390:
-        score_2 += 1
-        hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=(FONT, 24, "normal"))
-        winsound.PlaySound(SCORE_SOUND,  winsound.SND_ASYNC)
-        ball.goto(0, random.randint(-200, 200))
-        ball.dx = 0.40
+    def paddle_2_up():
+        move_up(paddle_2)
 
-    # collision with right wall
-    if ball.xcor() > 390:
-        score_1 += 1
-        hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=(FONT, 24, "normal"))
-        winsound.PlaySound(SCORE_SOUND, winsound.SND_ASYNC)
-        ball.goto(0, random.randint(-200, 200))
-        ball.dx = -0.40
+    def paddle_2_down():
+        move_down(paddle_2)
 
-    # collision with paddle 1
-    if ball.xcor() < -330 and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50:
-        ball.dx = 0.90
-        winsound.PlaySound(UP_DOWN_SOUND,  winsound.SND_ASYNC)
+    def exit_game():
+        turtle.clear()
+        turtle.bye()
 
-    # collision with paddle 2
-    if ball.xcor() > 330 and paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() - 50:
-        ball.dx = -0.90
-        winsound.PlaySound(UP_DOWN_SOUND,  winsound.SND_ASYNC)
+    # mapping keys
+    screen.listen()
+    screen.onkeypress(paddle_1_up, "w")
+    screen.onkeypress(paddle_1_down, "s")
+    screen.onkeypress(paddle_2_up, "Up")
+    screen.onkeypress(paddle_2_down, "Down")
+    screen.onkeypress(exit_game, "space")
+
+    while True:
+        screen.update()
+
+        # ball move
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
+
+        # collision with top wall
+        if ball.ycor() > 285:
+            ball.sety(285)
+            ball.dy *= -1
+            winsound.PlaySound(UP_DOWN_SOUND,  winsound.SND_ASYNC)
+
+        # collision with down wall
+        if ball.ycor() < -280:
+            ball.sety(-280)
+            ball.dy *= -1
+            winsound.PlaySound(UP_DOWN_SOUND,  winsound.SND_ASYNC)
+
+        # collision with left wall
+        if ball.xcor() < -390:
+            score_2 += 1
+            hud.clear()
+            hud.write("{} : {}".format(score_1, score_2), align="center", font=(FONT, 24, "normal"))
+            winsound.PlaySound(SCORE_SOUND,  winsound.SND_ASYNC)
+            ball.goto(0, random.randint(-200, 200))
+            ball.dx = 0.40
+
+        # collision with right wall
+        if ball.xcor() > 390:
+            score_1 += 1
+            hud.clear()
+            hud.write("{} : {}".format(score_1, score_2), align="center", font=(FONT, 24, "normal"))
+            winsound.PlaySound(SCORE_SOUND, winsound.SND_ASYNC)
+            ball.goto(0, random.randint(-200, 200))
+            ball.dx = -0.40
+
+        # collision with paddle 1
+        if ball.xcor() < -330 and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50:
+            ball.dx = 0.90
+            winsound.PlaySound(UP_DOWN_SOUND,  winsound.SND_ASYNC)
+
+        # collision with paddle 2
+        if ball.xcor() > 330 and paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() - 50:
+            ball.dx = -0.90
+            winsound.PlaySound(UP_DOWN_SOUND,  winsound.SND_ASYNC)
         
-    # win condition
-    if score_1 == score_win or score_2 == score_win:
-        score_1 = score_2 = 0
+        # win condition
+        if score_1 == score_win or score_2 == score_win:
+            score_1 = score_2 = 0
+
+
+if __name__ == '__main__':
+
+    start_page()
+    mainloop()
